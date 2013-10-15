7b4803c799daca8b14faa2fe999a4a8085b17594
Hugh Bien <hugh@hughbien.com>
2013-10-15 02:06:41 -0700

    Use auto-hinted fonts for better rendering on site.

--- site/controller.rb
+++ site/controller.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'homeostasis'
 
 Stasis::Options.set_template_option 'scss', Compass.sass_engine_options
+Compass.configuration do |config|
+  config.fonts_dir = 'font'
+end
 Homeostasis::Asset.concat 'all.css', %w(styles.css)
 Homeostasis::Sitemap.config(url: 'http://thymerb.com')
 
